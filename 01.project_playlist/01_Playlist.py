b265a42646a6e56d1ee2d7aeaaa546c12b4cb442
NghiepNC <nha.y.thienztu9@gmail.com>
2023-03-28 14:58:17 +0700

    Playlist full function with command line

--- 01.project_playlist/01_Playlist.py
+++ 01.project_playlist/01_Playlist.py
@@ -18,10 +18,17 @@ Class Playlist(năm,description,rating,videos)
 
 
 '''
+import webbrowser
+
 class Video:
     def __init__(self, title, link):
         self.title = title
         self.link = link
+        self.seen = False
+    
+    def open(self):
+        webbrowser.open(self.link)
+        self.seen = True
 
 class Playlist:
     def __init__(self, name, description, rating,videos):
@@ -51,6 +58,7 @@ def print_video(video):
     
 def print_videos (videos):
     for i in range(len(videos)):
+        print("Video "+ str(i+1) + ':')
         print_video(videos[i])
 
 def write_video(videos, file):
@@ -128,6 +136,9 @@ def show_menu():
     print("|Option 1: Create playlist   |")
     print("|Option 2: Show playlist     |")
     print("|Option 3: Play a video      |")
+    print("|Option 4: Add a video       |")
+    print("|Option 5: Update playlist   |")
+    print("|Option 6: Delete a video    |")
     print("|Option 7: Save and Exit     |")
     print("------------------------------")
 
@@ -142,6 +153,53 @@ def play_video(playlist):
                              + str(total)+ "): ", 1, total)
     print("Open video: " + playlist.videos[choice-1].title \
           + " - " + playlist.videos[choice-1].link , end = '')
+    # webbrowser.open(playlist.videos[choice-1].link)
+    playlist.videos[choice-1].open()
+
+def add_new_video(playlist):
+    print("Enter new song infomation: ")
+    new_title = input("New Title: ") + '\n'
+    new_link = input ("New Link: ") + '\n'
+    new_video = Video(new_title, new_link)
+    playlist.videos.append(new_video)
+    
+def update_playlist(playlist):
+    print("what's your update")
+    print("1. Update name playlist")
+    print("2. Update description playlist")
+    print("3. Update rating playlist")
+
+    choice = select_in_range("Choose option 1->3: ", 1,3)
+    if choice == 1:
+        new_name = input("Enter new name for playlist: ") + '\n'
+        playlist.name = new_name
+        print("Updated successfully !!")
+        return playlist
+    elif choice == 2:
+        new_description = input("Enter new description for playlist: ") + '\n'
+        playlist.description= new_description
+        print("Updated successfully !!")
+        return playlist
+    elif choice == 3:
+        new_rating = str(select_in_range("Enter new rating 1->5 : ",1,5)) + '\n'
+        playlist.rating = new_rating
+        print("Updated successfully !!")
+        return playlist
+def delete_video(playlist):
+    print_videos(playlist.videos)
+    choice =select_in_range("Enter video you want to delete: ", \
+                            1, len(playlist.videos))
+    # del playlist.videos[choice -1]  # function del 
+    new_videos_list = []
+    for i in range(len(playlist.videos)):
+        if i == choice -1: continue
+        new_videos_list.append(playlist.videos[i])
+    playlist.videos = new_videos_list
+    print("Deleted successfully!! ")
+    return playlist
+
+
+
 
 def main():
     
@@ -162,6 +220,15 @@ def main():
             input("\nPress Enter to continue.\n")
         elif choice == 3:
             play_video(playlist)
+            input("\nPress Enter to continue.\n")
+        elif choice == 4:
+            playlist = add_new_video(playlist)
+            input("\nPress Enter to continue.\n")
+        elif choice == 5:
+            playlist = update_playlist(playlist)
+            input("\nPress Enter to continue.\n")
+        elif choice == 6:
+            playlist = delete_video(playlist)
             input("\nPress Enter to continue.\n")
         elif choice == 7:
             write_playlist_txt(playlist)
